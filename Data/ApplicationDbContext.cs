bf6c6742dfece0c54428fdd32da458bd1fc12a6d
Long Kelvin <longkelvin101099@gmail.com>
2022-12-26 21:45:30 +0700

    git init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using Net7_Twilio_VideoChatApplication.Models;
+
+namespace Net7_Twilio_VideoChatApplication.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Net7_Twilio_VideoChatApplication.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
